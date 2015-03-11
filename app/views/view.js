55677e0ebd94d69168214ab9f3d2bd7bb4d79162
stevenb-nz & tojarra <pair+stevenb-nz.tojarra@devbootcamp.com>
2015-03-12 12:26:39 +1300

    View.placePiece(player,row,column) working

--- app/views/view.js
+++ app/views/view.js
@@ -1,7 +1,11 @@
 function View() {
 
   this.placePiece = function(player,row,column) {
-    cell = $( "table:nth-child(row):nth-child(column)" );
-    console.log($(cell));
+    cell = $( 'tr:nth-child('+row+') td:nth-child('+column+')' );
+    if (player == 1) {
+      $(cell).addClass('p1');
+    } else {
+      $(cell).addClass('p2');
+    }
   }
 }
