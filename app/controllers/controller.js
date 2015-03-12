c5412a8cb28a2e58ce5cc67d07b3b5790749131b
SandraMuller <mursan03@gmail.com>
2015-03-13 09:44:39 +1300

    model

---
+++ app/controllers/controller.js
@@ -0,0 +1,7 @@
+function Controller() {
+	var view = new View();
+	var model = new Model();
+
+	var turn = 0;
+
+}
--- app/models/model.js
+++ app/models/model.js
@@ -1,67 +1,79 @@
 function Model(){
 
-  function player(name,number){
-  this.name = name;
-  this.number = number;
-  this.has_won = false;
-} //player
+    function Player(name,number) {
+    this.name = name;
+    this.number = number;
+    this.has_won = false;
+    } //player
 
-  player1 = new player('GreenLantern', 1);
-  player2 = new player('Hulk', 2);
-
-
-  var board = [
-    [0,0,0,0,0,0,0],
-    [0,2,2,2,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0],
-    [0,0,1,1,1,1,0],
-    [0,0,0,0,0,0,0]
-  ];
-
-function drop(num, player){
-  for (var i = 5; i >= 0; i--)
-  {
-    if (board[i][num] === 0)
-    {
-      board[i][num] = player;
-      break;
-    }
-  }
-
-}
+    player1 = new Player('GreenLantern', 1);
+    player2 = new Player('Hulk', 2);
 
 
-console.log(board);
+      var board = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,2,0,0,0,0],
+        [0,0,2,0,0,0,0],
+        [0,0,2,0,0,0,0],
+        [0,0,1,1,1,1,0]
+      ];
 
-
-console.log(board);
-
-  function check(player){
-    var RowCount = 0
-    for (var i = 0; i < 6; i++) {
-      for (var j = 0; j < 7; j++) {
-        if (board[i][j] === player){
-          RowCount++;
-          console.log("row count",RowCount);
+    function drop(num, player){
+        for (var i = 5; i >= 0; i--)
+        {
+            if (board[i][num] === 0)
+            {
+              board[i][num] = player;
+              break;
+            }
         }
-          else {
-            RowCount = 0;
-          }
-        if(RowCount === 4){
-          if(player===1){player1.has_won = true;}
-          else {player2.has_won = true;}
-      }
-    }
-      console.log(player1.has_won, player2.has_won);
     }
 
+    function check(player){
+      //Match for Row
+        for (var i = 0; i < 6; i++) { 
+            for (var j = 0; j < 7; j++) {
+              var match = board[i][j];
+              matches(match, player);
+            }
+        }
+      //Match for Column
+        for (var i = 0; i < 7; i++) { 
+            for (var j = 0; j < 6; j++) {
+              var match = board[j][i];
+              matches(match, player);
+            }
+        }    
+    }
+
+    var count = 0
+    function matches(match, player) {
+      //Match cell against player number
+        if (match === player) {
+            count++;
+        } else {
+            count = 0;
+        }
+      //If the count has reached 4 someone has won!
+        if(count === 4) {
+            if(player === 1){player1.has_won = true;}
+            else {player2.has_won = true;}
+        }
+    }
+
+    function winner(){
+      if (player1.has_won === true){return player1.name+" has won!"};
+      if (player2.has_won === true){return player2.name+" has won!"};
+    }
 
 
-
-  } //check
+drop(1,2);
+console.log(board);
 check(1);
+check(2);
+console.log(winner());
 
 } //model
+
 x = new Model();
-// x.drop(1,2)
