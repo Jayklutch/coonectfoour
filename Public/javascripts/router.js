5d70d4a2fd2f2f163ca4550c9653f91054c3f566
stevenb-nz & tojarra <pair+stevenb-nz.tojarra@devbootcamp.com>
2015-03-12 15:49:45 +1300

    method in router to send column clicked-on to controller

---
+++ Public/javascripts/router.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+//  var controller = new Controller();
+  $('body').on('keyup', function(e) {
+    if (e.keyCode === 13) {
+      controller.play();
+    }
+  });
+  $('body').on('click', 'td', function(e) {
+    controller.columnClicked($(this).attr("class"));
+  });
+
+});
