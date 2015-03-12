526328d48169dc436482a2f6b88e767eef6abd91
Jayklutch & tojarra <pair+jayklutch.tojarra@devbootcamp.com>
2015-03-13 10:25:14 +1300

    tidy up model

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -5,3 +5,8 @@ function Controller() {
 	var turn = 0;
 
 }
+
+
+    player1 = new Player('GreenLantern', 1);
+    player2 = new Player('Hulk', 2);
+
--- app/models/model.js
+++ app/models/model.js
@@ -1,79 +1,64 @@
 function Model(){
 
-    function Player(name,number) {
-    this.name = name;
-    this.number = number;
-    this.has_won = false;
-    } //player
+  var Player function(name,number) {
+  this.name = name;
+  this.number = number;
+  this.has_won = false;
+  }
 
-    player1 = new Player('GreenLantern', 1);
-    player2 = new Player('Hulk', 2);
+  var board = [
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0]
+              ];
 
-
-      var board = [
-        [0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0],
-        [0,0,2,0,0,0,0],
-        [0,0,2,0,0,0,0],
-        [0,0,2,0,0,0,0],
-        [0,0,1,1,1,1,0]
-      ];
-
-    function drop(num, player){
-        for (var i = 5; i >= 0; i--)
-        {
-            if (board[i][num] === 0)
-            {
-              board[i][num] = player;
-              break;
-            }
-        }
+  this.drop function(num, player){
+    for (var i = 5; i >= 0; i--){
+      if (board[i][num] === 0){
+        board[i][num] = player;
+        return i+1;
+      }
     }
+    return 0
+  }
 
-    function check(player){
-      //Match for Row
-        for (var i = 0; i < 6; i++) { 
-            for (var j = 0; j < 7; j++) {
-              var match = board[i][j];
-              matches(match, player);
-            }
-        }
-      //Match for Column
-        for (var i = 0; i < 7; i++) { 
-            for (var j = 0; j < 6; j++) {
-              var match = board[j][i];
-              matches(match, player);
-            }
-        }    
+  Player.prototype.check function(player){
+    for (var i = 0; i < 6; i++){
+      for (var j = 0; j < 7; j++){
+        var match = board[i][j];
+        matches(match, player);
+      }
     }
+    for (var i = 0; i < 7; i++){
+      for (var j = 0; j < 6; j++){
+        var match = board[j][i];
+          matches(match, player);
+      }
+    }
+  }
 
-    var count = 0
-    function matches(match, player) {
-      //Match cell against player number
-        if (match === player) {
-            count++;
-        } else {
-            count = 0;
-        }
+  var count = 0
+    function matches(match, player){
+      if (match === player){
+        count++;
+      } else {
+        count = 0;
+      }
       //If the count has reached 4 someone has won!
-        if(count === 4) {
-            if(player === 1){player1.has_won = true;}
-            else {player2.has_won = true;}
+      if(count === 4){
+        if(player === 1){
+          player1.has_won = true;
+        } else {
+          player2.has_won = true;
         }
+      }
     }
 
     function winner(){
       if (player1.has_won === true){return player1.name+" has won!"};
       if (player2.has_won === true){return player2.name+" has won!"};
     }
-
-
-drop(1,2);
-console.log(board);
-check(1);
-check(2);
-console.log(winner());
-
-} //model
-
-x = new Model();
+}
